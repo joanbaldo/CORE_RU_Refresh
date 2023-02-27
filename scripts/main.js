f02dcf9b2cd3bcaefba3ff78dab3d58ff07e9cff
joanbaldo <joanbaldo@hotmail.com>
2023-02-27 13:59:12 +0100

    EX 2 Optional Validation

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ function multiplicacion(a, b) {
 console.log(multiplicacion(2, 5))
 */
 // Crea un programa que pida al usuario su edad y, dependiendo de la misma, muestre por consola uno de los siguientes mensajes: "Eres menor de edad", "Eres mayor de edad" o "Eres un adulto mayor". Debes utilizar un condicional if/else.
-
+/*
 function imserso(edad) {
     edad = prompt("Porfavor introduce tu edad");
     edad = +edad;
@@ -29,21 +29,52 @@ function imserso(edad) {
 
 }
 imserso()
-
-
-// let edad = 50
-// if (edad < 18) {
-//     console.log('Eres menor de edad');
-//         } else if (edad >=18 && edad <=40) {
-//     console.log('Eres mayor de edad');                
-//         } else {
-//              console.log('Eres un adulto mayor');
-            
-//         }
-
+*/
 // Crea la función sumaTresNumeros que reciba tres números como argumento y devuelva la suma de los mismos. Si alguno de los argumentos no es un número, debe devolver el string "Debo ser ejecutada con números".
 
+
+// opcion 1 "Consoleando en el própio return"
+
+function sumaTresNumerosa(a, b, c) {
+    a = prompt("Introduzca primer número");
+    a = +a; //-> forzamos a convertir "lo que sea" a num y si no puede =dara como resultado NaN así que si una de las tres variables se suma como "NaN" el resultado que se consoleara será "NaN"
+    console.log(typeof a)
+    b = prompt("Introduzca segundo número");
+    b = +b; //(ídem a)
+    console.log(typeof b)
+    c = prompt("Introduzca segundo número");
+    c = +c; //(ídem a)
+    console.log(typeof c)
+    console.log(isNaN(c))
+    if ((typeof a === 'number') && (typeof b === 'number') && (typeof c === 'number')) {
+        console.log(a + b + c);
+    } else {
+        console.log('Debo ser ejecutada con números')
+    }
+}
+sumaTresNumerosa()
+
+// opcion 2 "Consoleando al llamar a la función"
+
+// function sumaTresNumerosa(a, b, c) {
+//     a = prompt("Introduzca primer número");
+//     b = prompt("Introduzca segundo número");
+//     c = prompt("Introduzca segundo número");
+//     if ((typeof a === 'number') && (typeof b === 'number') && (typeof c === 'number')) {
+//         console.log(a + b + c);
+//     } else {
+//         return 'Debo ser ejecutada con números'1
+//     }
+// }
+// console.log(sumaTresNumerosa())
+
+
 // Crea la función primerosTresCaracteres que reciba un string como argumento y devuelva los tres primeros caracteres del mismo. Si el argumento no es un string, debe devolver el string "Debo ser ejecutada con un string". Si el string tiene menos de tres caracteres, debe devolver el string completo.
+
+// function primerosTresCaracteres(string) {
+
+    
+// }
 
 // Crea la función getPrecioMostrarIVA para que devuelva una cadena de texto con formato precio con dos decimales y con el IVA ya incluido. Para 2 debería devolver 2.42 € (suponiendo un IVA del 21%). Si la función no recibe un número debería devolver devolver 'no es un formato correcto'.
 
